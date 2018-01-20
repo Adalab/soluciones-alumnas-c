ce6025e76ef35052272fc84877e952b78155b206
Alienah <aidalbla@gmail.com>
2018-01-20 18:29:19 +0100

    Another solution of excersise 4.2-3?

--- sprint-4/4.2/4-2-ejercicio-3/js/main.js
+++ sprint-4/4.2/4-2-ejercicio-3/js/main.js
@@ -1,4 +1,38 @@
 'use strict';
+class Polygon{
+  constructor(numberOfSides, base, height){
+    this.numberOfSides = numberOfSides;
+    this.base = base;
+    this.height = height;
+  }
+  perimeter(){
+    return this.base * this.numberOfSides;
+  }
+  area(){
+    return this.base * this.height;
+    //return Math.pow(this.base,2);  Otra solución
+  }
+}
+class Square extends Polygon {
+  constructor(side){
+    super(4, side, side);
+  }
+}
+class Triangle extends Polygon{
+  constructor(base, heigth){
+    super(3, base, heigth);
+  }
+  area(){
+    return super.area() / 2;
+  }
+}
+const item1 = new Square(4);
+const item2 = new Triangle(4, 3);
+
+console.log(`El cuadrado de 4cm de lado tiene un périmetro de ${item1.perimeter()}cm y un área de ${item1.area()}cm²`);
+console.log(`El triángulo de 4cm de base y 3cm de altura tiene un périmetro de ${item2.perimeter()}cm y un área de ${item2.area()}cm²`);
+
+/*Otra solución
 class Polygon {
   constructor(numberOfSides, base, height) {
     this.numberOfSides = numberOfSides;
@@ -46,3 +80,4 @@ console.log('Triángulo de 4m. de base y 3 m. altura:')
 console.log(`Perímetro: ${triangle.perimeter()} m.`);
 console.log(`Área: ${triangle.area()} m².`);
 console.log('---')
+*/
