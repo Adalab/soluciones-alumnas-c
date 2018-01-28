30c39a42bf889cba5b4018c94c95af8383eb0265
Alienah <aidalbla@gmail.com>
2018-01-28 15:53:15 +0100

    Add folder sprint-4/4.5/ with the excersise for correction

---
+++ sprint-4/4.5/4-5-ejercicio-4/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Destiny from './components/Destiny.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Destiny />
+	  );
+  }
+}
+
+export default App;
